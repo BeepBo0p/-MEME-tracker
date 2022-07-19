9e8094af8e377cfdb22108e2ddc41f7ae731965a
BeepBo0p <henrikbvm2@gmail.com>
2022-07-19 18:12:06 +0200

    Changed paths

---
+++ api.py
@@ -0,0 +1,24 @@
+import tweepy
+import configparser
+import pandas as pd
+import json
+
+def twitter_setup():
+
+    # read configs
+    config = configparser.ConfigParser()
+    config.read('./resources/config.ini')
+
+    api_key = config['twitter']['api_key']
+    api_key_secret = config['twitter']['api_key_secret']
+
+    access_token = config['twitter']['access_token']
+    access_token_secret = config['twitter']['access_token_secret']
+
+
+    # authentication
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    return api
