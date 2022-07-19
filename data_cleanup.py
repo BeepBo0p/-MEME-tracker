cba7a2a0331ccb679987f0e1d7527f8f7a333b81
BeepBo0p <henrikbvm2@gmail.com>
2022-07-19 17:40:39 +0200

    Started data cleanup

---
+++ data_cleanup.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+# Function declaration
+
+def prune_data(dataframe: pd.DataFrame):
+    
+    return dataframe
+
+def add_to_database(dataframe: pd.DataFrame):
+    
+    return True # Return True if database upload is successful
+
+
+
+# Main program
+
+
+# Read tweets from file
+#TODO: Should probably create way to load directly from script without JSON intermediary
+raw_data_df = pd.read_json("./resources/tweets.json")
+
+# Drops all data not that shouldn't be kept according to specs on Trello
+index_drop_list = ["id",
+                   "truncated",
+                   "display_text_range",
+                   "source",
+                   "in_reply_to_status_id",
+                   "in_reply_to_status_id_str",
+                   "in_reply_to_user_id",
+                   "in_reply_to_user_id_str",
+                   "in_reply_to_screen_name",
+                   "geo",
+                   "coordinates",
+                   "place",
+                   "contributors",
+                   "retweeted_status",
+                   "is_quote_status",
+                   "favorited",
+                   "retweeted",
+                   "possibly_sensitive",
+                   "quoted_status_id",
+                   "quoted_status_id_str",
+                   "quoted_status"
+                    ]
+
+pruned_data_df = raw_data_df.drop(columns=index_drop_list)
+
+# Extracts entities and puts them in seperate dataframe
+
+entity_df = pd.DataFrame(list(pruned_data_df['entities']))
+
+#print(entity_df.columns)
+
+hashtag_df = pd.DataFrame(list(entity_df['hashtags']))
+symbols_df = pd.DataFrame(list(entity_df['symbols']))
+
+print(hashtag_df.columns)
+print(symbols_df.columns)
