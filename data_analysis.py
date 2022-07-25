80a3c917a1b736e6def41e2844b7170214149672
Henrik <henrrb@stud.ntnu.no>
2022-07-26 01:17:14 +0200

    Small fix of prop instead of fontsize

--- data_analysis.py
+++ data_analysis.py
@@ -118,30 +118,7 @@ def analyse_tweets():
     plt.pie(retweet_count, labels=user_name, autopct='%1.1f%%', textprops={'fontsize': 'x-small'}, startangle=90)   # Prosent   *Funker, men skal ikke være 100%
     # plt.pie(retweet_count, labels=user_name, autopct=lambda p: '{:.0f}'.format(p * total / 100), startangle=90)   # Tall      *Funker ikke
     plt.title('Top five Tweets by retweets')
-    plt.legend(title='Users', loc='upper left', fontsize='x-small')   
+    plt.legend(title='Users', loc='upper left', prop={'size': 'x-small'})   
     plt.show()
 
 analyse_tweets()
-
-# index_drop_list = ["id",
-#                    "truncated",
-#                    "display_text_range",
-#                    "source",
-#                    "in_reply_to_status_id",
-#                    "in_reply_to_status_id_str",
-#                    "in_reply_to_user_id",
-#                    "in_reply_to_user_id_str",
-#                    "in_reply_to_screen_name",
-#                    "geo",
-#                    "coordinates",
-#                    "place",
-#                    "contributors",
-#                    "retweeted_status",
-#                    "is_quote_status",
-#                    "favorited",
-#                    "retweeted",
-#                    "possibly_sensitive",
-#                    "quoted_status_id",
-#                    "quoted_status_id_str",
-#                    "quoted_status"
-#                     ]
